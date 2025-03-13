77411dac0fefbc73e6c66fe9eb3154d7bfcbe37f
aanh1009 <131883807+aanh1009@users.noreply.github.com>
2025-03-13 16:52:54 -0400

    Update classify.py

--- classify.py
+++ classify.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 from PIL import Image
 import torch
+
+#load saved model weights
 def load_data():
     with open('saved_steps.pkl','rb') as file:
         data = pickle.load(file)
@@ -10,9 +12,12 @@ def load_model():
     with open('model_scripted.pt','rb') as f:
         model = torch.jit.load('model_scripted.pt')
     return model
+    
+#load saved function to transform input images
 data_transforms = load_data()
 model = load_model()
 
+#structure the streamlit interface to take in images and output classification
 def show_classify():
     st.set_page_config(layout="centered")
     st.title('Traffic Signs Classification')
